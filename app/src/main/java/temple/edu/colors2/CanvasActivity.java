2d0ffab87d5ba5732d979c2f09a6370e9d7dee02
Francesko <Franceskoracaku@hotmail.com>
2020-02-25 18:59:15 -0500

    Final commit

---
+++ app/src/main/java/temple/edu/colors2/CanvasActivity.java
@@ -0,0 +1,25 @@
+package temple.edu.colors2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CanvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas);
+
+        Intent intent = getIntent();
+        String colorText = intent.getStringExtra(PaletteActivity.INTENT_COLOR_KEY);
+
+        findViewById(R.id.canvasLayout).setBackgroundColor(Color.parseColor(colorText));
+
+        TextView colorTextView = findViewById(R.id.colorText);
+        colorTextView.setText(colorText);
+    }
+}
---
+++ app/src/main/java/temple/edu/colors2/PaletteActivity.java
@@ -0,0 +1,61 @@
+package temple.edu.colors2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class PaletteActivity extends AppCompatActivity {
+
+    public static final String INTENT_COLOR_KEY = "color";
+    private boolean firstSelection = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palette);
+
+        final ConstraintLayout c = findViewById(R.id.layout);
+
+        Spinner s;
+        s = findViewById(R.id.spinner);
+
+        String colors[] = {
+                "Blue",
+                "Red",
+                "White",
+                "Yellow",
+                "Green",
+                "Aqua",
+                "Olive",
+                "Lime",
+                "Cyan",
+                "Silver"
+        };
+
+        ColorAdapter a = new ColorAdapter(this, colors);
+        s.setAdapter(a);
+
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(!firstSelection) {   //Prevent onItemSelected() from running this code automatically (i.e. before the user chooses anything)
+                    Intent launchIntent = new Intent(PaletteActivity.this, CanvasActivity.class);
+                    launchIntent.putExtra(INTENT_COLOR_KEY, parent.getItemAtPosition(position).toString());
+                    startActivity(launchIntent);
+                } else {
+                    firstSelection = false;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+}
